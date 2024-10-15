c7bf4e6df596212c54411c79dcff5d8fbf3ab669
Simone Guggiari <sguggiari@roblox.com>
2024-10-15 15:21:20 +0200

    getids cleanup

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ This repository contains the Python source code in the `/src` folder. The `.exe`
 
 ### Main Fork Changes
 This fork introduces the following main changes from the original [juanjo4martinez/mixamo-downloader](https://github.com/juanjo4martinez/mixamo-downloader):
-- updates `mixamo_anims.json` to contain all animations as of October 14th, 2024, which is 100 more than the original repo.
+- updates `mixamo_anims.json` to contain all animations as of October 14th, 2024, which is 100 more than the original repo, for a total of **2446**.
 - provides a script `getids.py` to scrape all new animations and create updated `mixamo_anims.json`
 - fixes an error that was causing the script to getting stuck on a few animations. If an animation cannot be downloaded after 10 tries, it's logged as a warning and skipped.
+   - currently, the following 3 animations seem to be non-downloadable: `c9c7386d-b96c-11e4-a802-0aaa78deedf9`, `c9c936bd-b96c-11e4-a802-0aaa78deedf9`, `fc6ff619-4203-40a8-b19b-b0d0712ddf0f`
 - prints errors, if any, in the console
 - saves files as `{index}_{anim_desc}.fbx`, to avoid animations with the same description from overwriting each other
-- has a more robust request mechanism (inspired from [jonnytracker/mixamo-downloader](https://github.com/jonnytracker/mixamo-downloader/tree/patch-1)
-- sanitizes animation names when saving to prevent errors (inspired from [ALK222/mixamo-downloader](https://github.com/ALK222/mixamo-downloader/tree/main)
+- has a more robust request mechanism (inspired from [jonnytracker/mixamo-downloader](https://github.com/jonnytracker/mixamo-downloader/tree/patch-1))
+- sanitizes animation names when saving to prevent errors (inspired from [ALK222/mixamo-downloader](https://github.com/ALK222/mixamo-downloader/tree/main))
 
 ### Running the script
 
--- src/getids.py
+++ src/getids.py
@@ -2,7 +2,6 @@ import json
 import os
 import requests
 import time
-from PySide2 import QtCore, QtWebEngineWidgets, QtWidgets
 
 HEADERS = {
     "Accept": "application/json",
@@ -24,11 +23,10 @@ def make_request(method, url, **kwargs):
             continue
     raise Exception(f"Failed to complete request to {url} after 10 retries.")
 
-def get_queried_animations_data():
-    page_num = 0
+def get_animations_data():
     params = {
       "limit": 96,
-      "page": page_num,
+      "page": 0,
       "type": "Motion",
       #"query": query,
     }
@@ -37,28 +35,26 @@ def get_queried_animations_data():
     num_pages = data["pagination"]["num_pages"]
     animations = []
 
-    #num_pages = 3
     print(f"numpages={num_pages}")
 
-    for _ in range(20):
-        page_num = 0
-        while page_num <= num_pages:
-            print(f"page={page_num}")
+    # for some reason, it seems not all animations are returned in a single pass.
+    # Therefore we run the query multiple times and then de-duplicate to ensure all animations are included.
+    runs = 20
+    for run in range(runs):
+        print(f'run={run+1}/{runs}')
+        for page_num in range(num_pages+1):
+            print(f"  page={page_num}")
             params["page"] = page_num
             response = make_request("GET", "https://www.mixamo.com/api/v1/products", headers=HEADERS, params=params)
             data = response.json()
-            #print(len(data["results"])) # 96
-            #print(data["results"])
             animations.extend(data["results"])
-            page_num += 1
-        print(f"total={len(animations)}")
+        print(f"  total={len(animations)}")
 
-    #anim_data = [f'{animation["motion_id"]}{animation["description"]}' for animation in animations]
     anim_data = {animation["id"]: animation["description"] for animation in animations}
     return anim_data
 
-ans = get_queried_animations_data()
-print(f"found {len(ans)}")
+ans = get_animations_data()
+print(f"found {len(ans)} unique animations")
 
 with open("ans.json", 'w') as json_file:
     json.dump(ans, json_file, indent=4)  # `indent=4` for pretty printing
