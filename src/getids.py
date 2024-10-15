7dae5280c79199ccb18cac591d0203c673866b48
Simone Guggiari <sguggiari@roblox.com>
2024-10-15 18:45:25 +0200

    added table with all animations and media

--- src/getids.py
+++ src/getids.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 import time
+import re
 
 HEADERS = {
     "Accept": "application/json",
@@ -50,11 +51,31 @@ def get_animations_data():
             animations.extend(data["results"])
         print(f"  total={len(animations)}")
 
-    anim_data = {animation["id"]: animation["description"] for animation in animations}
+    anim_data = {animation["id"]: animation for animation in animations}
     return anim_data
 
-ans = get_animations_data()
-print(f"found {len(ans)} unique animations")
+def make_table(animations):
+    with open('animations_table.md', 'w') as file:
+        file.write(f"| ID | Name | Description | Media |\n")
+        file.write(f"|----|------|-------------|-------|\n")
+        for id, animation in animations.items():
+            assert animation['type'] == 'Motion'
+            assert animation['category'] == ''
+            assert animation['character_type'] == 'human'
+            media_id = re.search(r'/motions/(\d+)/', animation['thumbnail']).group(1)
+            assert animation['thumbnail'] == f'https://d99n9xvb9513w.cloudfront.net/thumbnails/motions/{media_id}/static.png'
+            assert animation['thumbnail_animated'] == f'https://d99n9xvb9513w.cloudfront.net/thumbnails/motions/{media_id}/animated.gif'
+            assert animation['motion_id'] == animation['id']
+            assert animation['motions'] == None
+            assert animation['source'] == 'system'
+            file.write(f"| {animation['id']} | {animation['name']} | {animation['description']} | {media_id} [PNG]({animation['thumbnail']}) [GIF]({animation['thumbnail_animated']}) |\n")
 
-with open("ans.json", 'w') as json_file:
-    json.dump(ans, json_file, indent=4)  # `indent=4` for pretty printing
+
+animations = get_animations_data()
+print(f"found {len(animations)} unique animations")
+
+anim_ans = {anim['id']: anim['description'] for id, anim in animations.items()}
+with open("mixamo_anims_new.json", 'w') as json_file:
+    json.dump(anim_ans, json_file, indent=4)  # `indent=4` for pretty printing
+
+make_table(animations)
